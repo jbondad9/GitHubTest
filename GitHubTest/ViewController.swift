b0498ad9b13a9269347cdb5a1681056a88c6ddea
Juan-Alejandro 2019 <jbondad9@3012-l29548-s.local>
2018-09-12 09:45:31 -0500

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Juan-Alejandro Bondad 2019 on 9/12/18.
+//  Copyright © 2018 Juan-Alejandro Bondad 2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
